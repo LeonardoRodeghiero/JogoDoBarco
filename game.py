6b52bc4e9b3b14635e74c16f0bf36d58800cdab9
unknown <rodeghieroleonardo@gmail.com>
2025-06-06 17:07:12 -0300

    Melhora na animacao da moeda

--- game.py
+++ game.py
@@ -101,22 +101,41 @@ class Moeda(pygame.sprite.Sprite):
         super().__init__()
 
         if tipo == 'ouro':
-            ouro_1 = pygame.image.load('moeda/moedaourofrente.png').convert_alpha()
-            ouro_2 = pygame.image.load('moeda/moedaourolado.png').convert_alpha()
-            self.frames = [ouro_1, ouro_2]
+            ouro_1 = pygame.image.load('moeda/ouro/moedaouro1.png').convert_alpha()
+            ouro_2 = pygame.image.load('moeda/ouro/moedaouro2.png').convert_alpha()
+            ouro_3 = pygame.image.load('moeda/ouro/moedaouro3.png').convert_alpha()
+            ouro_4 = pygame.image.load('moeda/ouro/moedaouro4.png').convert_alpha()
+            ouro_5 = pygame.image.load('moeda/ouro/moedaouro5.png').convert_alpha()
+            ouro_6 = pygame.image.load('moeda/ouro/moedaouro6.png').convert_alpha()
+            ouro_7 = pygame.image.load('moeda/ouro/moedaouro7.png').convert_alpha()
+            ouro_8 = pygame.image.load('moeda/ouro/moedaouro8.png').convert_alpha()
+            
+            self.frames = [ouro_1, ouro_2, ouro_3, ouro_4, ouro_5, ouro_6, ouro_7, ouro_8]
             self.gravidade = randint(5, 7)
             self.tipo = 'ouro'
         elif tipo == 'prata':
-            prata_1 = pygame.image.load('moeda/moedapratafrente.png').convert_alpha()
-            prata_2 = pygame.image.load('moeda/moedapratalado.png').convert_alpha()
-            self.frames = [prata_1, prata_2]
+            prata_1 = pygame.image.load('moeda/prata/moedaprata1.png').convert_alpha()
+            prata_2 = pygame.image.load('moeda/prata/moedaprata2.png').convert_alpha()
+            prata_3 = pygame.image.load('moeda/prata/moedaprata3.png').convert_alpha()
+            prata_4 = pygame.image.load('moeda/prata/moedaprata4.png').convert_alpha()
+            prata_5 = pygame.image.load('moeda/prata/moedaprata5.png').convert_alpha()
+            prata_6 = pygame.image.load('moeda/prata/moedaprata6.png').convert_alpha()
+            prata_7 = pygame.image.load('moeda/prata/moedaprata7.png').convert_alpha()
+            prata_8 = pygame.image.load('moeda/prata/moedaprata8.png').convert_alpha()
+            self.frames = [prata_1, prata_2, prata_3, prata_4, prata_5, prata_6, prata_7, prata_8]
             self.gravidade = randint(3, 5)
             self.tipo = 'prata'
 
         elif tipo == 'bronze':
-            bronze_1 = pygame.image.load('moeda/moedabronzefrente.png').convert_alpha()
-            bronze_2 = pygame.image.load('moeda/moedabronzelado.png').convert_alpha()
-            self.frames = [bronze_1, bronze_2]
+            bronze_1 = pygame.image.load('moeda/bronze/moedabronze1.png').convert_alpha()
+            bronze_2 = pygame.image.load('moeda/bronze/moedabronze2.png').convert_alpha()
+            bronze_3 = pygame.image.load('moeda/bronze/moedabronze3.png').convert_alpha()
+            bronze_4 = pygame.image.load('moeda/bronze/moedabronze4.png').convert_alpha()
+            bronze_5 = pygame.image.load('moeda/bronze/moedabronze5.png').convert_alpha()
+            bronze_6 = pygame.image.load('moeda/bronze/moedabronze6.png').convert_alpha()
+            bronze_7 = pygame.image.load('moeda/bronze/moedabronze7.png').convert_alpha()
+            bronze_8 = pygame.image.load('moeda/bronze/moedabronze8.png').convert_alpha()
+            self.frames = [bronze_1, bronze_2, bronze_3, bronze_4, bronze_5, bronze_6, bronze_7, bronze_8]
             self.gravidade = randint(2, 4)
             self.tipo = 'bronze'
 
